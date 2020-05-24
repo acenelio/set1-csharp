498e7b082aa000d961e09791e6a94dbde288af4c
Nelio Alves <acenelio@gmail.com>
2020-05-24 15:37:33 -0300

    Initial implementation

---
+++ Course/Entities/LogRecord.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Course.Entities {
+    class LogRecord {
+
+        public string Username { get; set; }
+        public DateTime Instant { get; set; }
+
+        public override int GetHashCode() {
+            return Username.GetHashCode();
+        }
+
+        public override bool Equals(object obj) {
+            if (!(obj is LogRecord)) {
+                return false;
+            }
+            LogRecord other = obj as LogRecord;
+            return Username.Equals(other.Username);
+        }
+    }
+}
